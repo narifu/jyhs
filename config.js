5e8df5d52172133c2df1d29b8b1e8335a12bbe65
narifu <narifu@163.com>
2018-01-07 10:18:56 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    auth: {},
+    authKey: 'mNo5sPAW5WyHq8ahtOrSV0mSbULQmZBT',
+    mySql: {
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'jyhs'
+    },
+    errorMessage: '数据库异常请联系管理员。',
+    ok: {"status":"ok"},
+    image:'/root/ui_jyhs/image/uploads',
+    bill:'/root/ui_jyhs/bill/download',
+    user:'/root/ui_jyhs/image/user/'
+};
+
